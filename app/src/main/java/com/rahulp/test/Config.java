b587739304af1d2bbdcd1cc2ae064b8dcc078ff3
Ranga <5110925+mkranga@users.noreply.github.com>
2024-09-02 23:51:16 +0530

    Update Config.java

--- app/src/main/java/com/rahulp/test/Config.java
+++ app/src/main/java/com/rahulp/test/Config.java
@@ -1,6 +1,6 @@
 package com.rahulp.test;
 
 public class Config {
-    public static final String EMAIL ="XXXXX"; //your-gmail-username
+    public static final String EMAIL ="XXXX1X"; //your-gmail-username
     public static final String PASSWORD ="XXXXX"; //your-gmail-password
 }
